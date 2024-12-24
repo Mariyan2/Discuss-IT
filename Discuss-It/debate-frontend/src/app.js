735fca997f2a24a9103546c013c77d5293906dc5
Mariyan <kitsune1@abv.bg>
2024-12-24 19:47:54 +0200

    Removed submodule and added debate-frontend as a normal folder

---
+++ Discuss-It/debate-frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/debate_page_components/header";
+
+function App() {
+    return (
+      <div>
+        <Header title="Welcome to React!" />
+      </div>
+    );
+  }
+  
+  export default App;
