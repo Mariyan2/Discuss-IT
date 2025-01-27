39cbbfaab63d2d804d3cbdcec9fd4448d0bde617
D2S03 <mariyan_lambov@abv.bg>
2025-01-28 01:41:57 +0200

    video containers adjusted and they both now have handles

--- src/pages/DiscussionPage.jsx
+++ src/pages/DiscussionPage.jsx
@@ -26,62 +26,69 @@ const Discussion = () => {
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-    {/* Header */}
-    <div className="p-4">
-      <div className="absolute top-4 left-4">
-        <Link to="/">
-          <img
-            src={require("../images/thumbnail.png")}
-            alt="Home"
-            style={{ width: "200px", height: "176px" }}
-            className="cursor-pointer"
-          />
-        </Link>
+      {/* Header Section */}
+      <div className="p-4 relative">
+        <div className="absolute top-4 left-4">
+          <Link to="/">
+            <img
+              src={require("../images/thumbnail.png")}
+              alt="Home"
+              style={{ width: "200px", height: "176px" }}
+              className="cursor-pointer"
+            />
+          </Link>
+        </div>
+        {discussion ? (
+          <Header title={discussion.topic} />
+        ) : (
+          <h2 className="text-center text-xl">Loading discussion...</h2>
+        )}
       </div>
-      {discussion ? (
-        <Header title={discussion.topic} />
-      ) : (
-        <h2 className="text-center text-xl">Loading discussion...</h2>
-      )}
-    </div>
-  
-    {/* Approval Bar */}
-    <div className="p-4">
-      <ApprovalBar percentage={discussion ? discussion.leftAgreeRatio : 0} />
-    </div>
-  
-    {/* Main Video and Chat Section */}
-    <div className="flex flex-1 flex-col">
-      {/* Grouped Video Containers */}
-      <div className="flex flex-row justify-between p-4 space-x-4">
-        {/* Left Video Container */}
-        <div className="flex-1 flex flex-col bg-gray-200 rounded p-4">
-          <VideoContainer />
-        </div>
-  
-        {/* Right Video Container */}
-        <div className="flex-1 flex flex-col bg-gray-200 rounded p-4">
-          <VideoContainer />
-        </div>
-  
-        {/* Chat Section */}
-        <div className="w-1/3 flex flex-col bg-gray-100 rounded p-4">
-          {discussion ? (
-            <ChatApp discussionId={discussionId} chatMessages={discussion.chat || []} />
-          ) : (
-            <p>Loading chat...</p>
-          )}
-        </div>
-      </div>
-  
-      {/* Viewers Section */}
+
+      {/* Approval Bar Section */}
       <div className="p-4">
-        <Handles />
-        <Viewers />
+        <ApprovalBar percentage={discussion ? discussion.leftAgreeRatio : 0} />
+      </div>
+
+      {/* Main Content Section */}
+      <div className="flex flex-col p-4 space-y-4">
+        <div className="flex space-x-4">
+          {/* Left Video Container */}
+          <div className="flex-1 flex flex-col bg-gray-200 rounded p-4">
+            <h3 className="text-center font-bold text-2xl mb-4">JordaN</h3>
+            <VideoContainer />
+            {/* // Added Handles Component for Left Video */}
+            <div className="mt-4">
+              <Handles />
+            </div>
+          </div>
+
+          {/* Right Video Container */}
+          <div className="flex-1 flex flex-col bg-gray-200 rounded p-4">
+            <h3 className="text-center font-bold text-2xl mb-4">Richard1</h3>
+            <VideoContainer />
+            {/* // Added Handles Component for Right Video */}
+            <div className="mt-4">
+              <Handles />
+            </div>
+          </div>
+
+          {/* Chat Section */}
+          <div className="w-1/3 bg-gray-100 rounded p-4">
+            {discussion ? (
+              <ChatApp discussionId={discussionId} chatMessages={discussion.chat || []} />
+            ) : (
+              <p>Loading chat...</p>
+            )}
+          </div>
+        </div>
+
+        {/* Viewers Section */}
+        <div className="bg-gray-200 rounded p-4">
+          <Viewers />
+        </div>
       </div>
     </div>
-  </div>
-  
   );
 };
 
